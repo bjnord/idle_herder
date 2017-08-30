ab2e06e5f4630a9061491df5c2b809a760b200d2
Brent J. Nordquist <brent@nordist.net>
2017-08-30 15:28:59 -0500

    Define faction/class arrays

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,9 +1,12 @@
 class Hero < ApplicationRecord
+  FACTIONS = ['Forest', 'Shadow', 'Fortress', 'Abyss', 'Light', 'Dark']
+  CLASSES = ['Warrior', 'Mage', 'Assassin', 'Ranger', 'Priest']
+
   validates :name, presence: true
   validates :stars, presence: true
   validates :stars, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 10 }, unless: Proc.new {|h| h.stars.blank? }
   validates :faction, presence: true
-  validates :faction, inclusion: {in: ['Forest', 'Shadow', 'Fortress', 'Abyss', 'Light', 'Dark'], message: 'is not valid'}, unless: Proc.new {|h| h.faction.blank? }
+  validates :faction, inclusion: {in: FACTIONS, message: 'is not valid'}, unless: Proc.new {|h| h.faction.blank? }
   validates :class, presence: true
-  validates :class, inclusion: {in: ['Warrior', 'Mage', 'Assassin', 'Ranger', 'Priest'], message: 'is not valid'}, unless: Proc.new {|h| h.class.blank? }
+  validates :class, inclusion: {in: CLASSES, message: 'is not valid'}, unless: Proc.new {|h| h.class.blank? }
 end
