c60b1f2a426f132f4804a46acc9eec470bf78047
Brent J. Nordquist <brent@nordist.net>
2017-09-02 19:35:09 -0500

    About and Contact Us pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'heroes#index'
   devise_for :users
+  get 'about', to: 'about#index'
+  get 'about/contact'
+
   resources :heroes, only: [:index, :show]
 end
