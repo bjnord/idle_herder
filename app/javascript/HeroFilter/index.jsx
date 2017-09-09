caa47dc5d17656635fc8b70315bb53af96709437
Brent J. Nordquist <brent@nordist.net>
2017-09-09 16:18:40 -0500

    React-driven smart bar: Filter on faction

--- app/javascript/HeroFilter/index.jsx
+++ app/javascript/HeroFilter/index.jsx
@@ -10,20 +10,47 @@ function matchesAnyPattern(str, patterns)
   return patterns.some((regexp) => str.match(regexp));
 }
 
+function matchesToggles(index, toggles)
+{
+  if (Object.keys(toggles).length < 1) {
+    return true;
+  }
+  if (index in toggles) {
+    return toggles[index];
+  }
+  return false;
+}
+
 function staticHeroList()
 {
   return [
-    {id: 23, name: "Bonecarver", faction: "Shadow", stars: 4},
-    {id: 24, name: "Bonecarver", faction: "Shadow", stars: 5},
-    {id: 25, name: "Bonecarver", faction: "Shadow", stars: 6},
-    {id: 217, name: "Fat Mu", faction: "Abyss", stars: 5},
-    {id: 218, name: "Fat Mu", faction: "Abyss", stars: 6},
-    {id: 225, name: "Gusta", faction: "Abyss", stars: 5},
-    {id: 226, name: "Gusta", faction: "Abyss", stars: 6},
-    {id: 392, name: "Logan", faction: "Dark", stars: 4},
-    {id: 393, name: "Logan", faction: "Dark", stars: 5},
-    {id: 414, name: "Fegan", faction: "Light", stars: 4},
-    {id: 415, name: "Fegan", faction: "Light", stars: 5},
+    {id: 23, name: 'Bonecarver', faction: 0, stars: 4},
+    {id: 24, name: 'Bonecarver', faction: 0, stars: 5},
+    {id: 25, name: 'Bonecarver', faction: 0, stars: 6},
+    {id: 32, name: 'Dark Priest', faction: 0, stars: 3},
+    {id: 82, name: 'Shirley', faction: 0, stars: 3},
+    {id: 103, name: 'Fire Fist', faction: 1, stars: 3},
+    {id: 161, name: 'Reggie', faction: 1, stars: 3},
+    {id: 180, name: 'Storm Hudde', faction: 1, stars: 4},
+    {id: 181, name: 'Storm Hudde', faction: 1, stars: 5},
+    {id: 184, name: 'Time Mage', faction: 1, stars: 4},
+    {id: 185, name: 'Time Mage', faction: 1, stars: 5},
+    {id: 217, name: 'Fat Mu', faction: 2, stars: 5},
+    {id: 218, name: 'Fat Mu', faction: 2, stars: 6},
+    {id: 225, name: 'Gusta', faction: 2, stars: 5},
+    {id: 226, name: 'Gusta', faction: 2, stars: 6},
+    {id: 343, name: 'Starlight', faction: 3, stars: 5},
+    {id: 344, name: 'Starlight', faction: 3, stars: 6},
+    {id: 349, name: 'Sybil', faction: 3, stars: 3},
+    {id: 350, name: 'Thale', faction: 3, stars: 4},
+    {id: 351, name: 'Thale', faction: 3, stars: 5},
+    {id: 352, name: 'Thale', faction: 3, stars: 6},
+    {id: 363, name: 'Wind Walker', faction: 3, stars: 4},
+    {id: 364, name: 'Wind Walker', faction: 3, stars: 5},
+    {id: 392, name: 'Logan', faction: 4, stars: 4},
+    {id: 393, name: 'Logan', faction: 4, stars: 5},
+    {id: 414, name: 'Fegan', faction: 5, stars: 4},
+    {id: 415, name: 'Fegan', faction: 5, stars: 5},
   ];
 }
 
@@ -35,7 +62,7 @@ export default class HeroFilter extends React.Component
     this.state = {
       // FIXME when we switch to fetch, this should be []
       heroes: staticHeroList(),
-      smartBarFilters: {text: '', namePatterns: []},
+      smartBarFilters: {text: '', namePatterns: [], factionToggles: {}},
     };
     this.handleFilterChange = this.handleFilterChange.bind(this);
   }
@@ -46,7 +73,9 @@ export default class HeroFilter extends React.Component
       if (!matchesAnyPattern(hero.name, this.state.smartBarFilters.namePatterns)) {
         return false;
       }
-      // TODO filter factions
+      if (!matchesToggles(hero.faction, this.state.smartBarFilters.factionToggles)) {
+        return false;
+      }
       // TODO filter stars
       return true;
     });
@@ -90,5 +119,6 @@ export default class HeroFilter extends React.Component
     //console.debug('text="' + filters.text + '"');
     //let patterns = filters.namePatterns.map((regexp) => regexp.toString());
     //console.debug('namePatterns=[' + patterns.join(' | ') + ']');
+    //console.debug('factionToggles=[' + Object.keys(filters.factionToggles).join(',') + ']');
   }
 }
--- app/javascript/HeroSmartBar/SmartTextParser.js
+++ app/javascript/HeroSmartBar/SmartTextParser.js
@@ -2,26 +2,39 @@ export default class SmartTextParser
 {
   constructor(text)
   {
-    this.filters = {text: text};
+    this.filters = {text: text, namePatterns: [], factionToggles: {}};
     this.parse();
   }
 
   wordToRegExp(word)
   {
     let patt = '\\b' + word.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-    //console.debug('patt=[' + patt + ']');
     return new RegExp(patt, 'i');
+  }
+
+  extractFactions(tokens)
+  {
+    return tokens.filter((token) => {
+      if (token.length == 1) {
+        // sadly, "Fortress" and "Forest" overlap significantly
+        // so "W" ("Wood") can be used for Forest
+        let f = 'SFAWDL'.indexOf(token.toUpperCase());
+        if (f >= 0) {
+          this.filters.factionToggles[f] = true;
+          return false;
+        }
+      }
+      return true;
+    });
   }
 
   parse()
   {
     let text = this.filters.text.trim();
-    if (!text) {
-      this.filters.namePatterns = [];
-    } else {
+    if (text) {
       let tokens = text.split(/\s+/);
       // TODO extract "quoted names" to filters.namePatterns
-      // TODO extract factions to filters.factions
+      tokens = this.extractFactions(tokens);
       // TODO extract stars to filters.stars
       // what's left are name words; turn them into regexes:
       // FIXME later this will have to append to the array, not initialize it
