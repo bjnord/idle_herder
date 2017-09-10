016e7163ce66a7d3192d9c48f7b39883f904e397
Brent J. Nordquist <brent@nordist.net>
2017-09-10 17:26:50 -0500

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {
-    host: ENV.fetch('PROD_SERVER_NAME') { 'idleherder.com' }
+    host: ENV.fetch('PROD_SERVER_NAME') { 'idleherder.com' },
     protocol: ENV['NO_FORCE_SSL'] ? 'http' : 'https'
   }
   if ENV['POSTMARK_USER_PASS']
