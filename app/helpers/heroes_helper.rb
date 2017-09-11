bfb7c8b579b39c41aa153fd582a59f68856d13db
Brent J. Nordquist <brent@nordist.net>
2017-09-11 18:17:17 -0500

    RF: Divide into partials for heroes#show

--- app/helpers/heroes_helper.rb
+++ app/helpers/heroes_helper.rb
@@ -2,4 +2,9 @@ module HeroesHelper
   def fmt_name(hero)
     "#{hero.name}#{hero.natural ? '‹n›' : ''}"
   end
+
+  def fmt_subtitle(hero)
+    natural_label = (hero.natural && (hero.stars == 5)) ? 'Natural ' : ''
+    "#{natural_label}#{hero.stars}★ #{hero.faction_name} #{hero.role}"
+  end
 end
