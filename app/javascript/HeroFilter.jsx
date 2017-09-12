2a03996a16c2d8aa44c63d0bbed7002b5d3eb765
Brent J. Nordquist <brent@nordist.net>
2017-09-12 05:46:58 -0500

    RF: Extract HeroList class

--- app/javascript/HeroFilter.jsx
+++ app/javascript/HeroFilter.jsx
@@ -1,76 +1,8 @@
+import HeroList from 'HeroList.js';
+
 import React from 'react';
-import HeroSmartBar from 'HeroSmartBar.jsx';
 import HeroBox from 'HeroBox.jsx';
-
-function matchesAnyPattern(str, patterns)
-{
-  if (patterns.length < 1) {
-    return true;
-  }
-  return patterns.some((regexp) => str.match(regexp));
-}
-
-function matchesToggles(index, toggles)
-{
-  if (Object.keys(toggles).length < 1) {
-    return true;
-  }
-  if (index in toggles) {
-    return toggles[index];
-  }
-  return false;
-}
-
-function staticHeroList()
-{
-  return [
-    {id: 13, name: "Blood Blade", faction: 0, stars: 5},
-    {id: 14, name: "Blood Blade", faction: 0, stars: 6},
-    {id: 15, name: "Blood Blade", faction: 0, stars: 7},
-    {id: 16, name: "Blood Blade", faction: 0, stars: 8},
-    {id: 17, name: "Blood Blade", faction: 0, stars: 9},
-    {id: 18, name: "Blood Blade", faction: 0, stars: 10},
-    {id: 23, name: 'Bonecarver', faction: 0, stars: 4},
-    {id: 24, name: 'Bonecarver', faction: 0, stars: 5},
-    {id: 25, name: 'Bonecarver', faction: 0, stars: 6},
-    {id: 32, name: 'Dark Priest', faction: 0, stars: 3},
-    {id: 82, name: 'Shirley', faction: 0, stars: 3},
-    {id: 103, name: 'Fire Fist', faction: 1, stars: 3},
-    {id: 117, name: "Iceblink", faction: 1, stars: 5},
-    {id: 118, name: "Iceblink", faction: 1, stars: 6},
-    {id: 119, name: "Iceblink", faction: 1, stars: 7},
-    {id: 120, name: "Iceblink", faction: 1, stars: 8},
-    {id: 121, name: "Iceblink", faction: 1, stars: 9},
-    {id: 122, name: "Iceblink", faction: 1, stars: 10},
-    {id: 161, name: 'Reggie', faction: 1, stars: 3},
-    {id: 180, name: 'Storm Hudde', faction: 1, stars: 4},
-    {id: 181, name: 'Storm Hudde', faction: 1, stars: 5},
-    {id: 184, name: 'Time Mage', faction: 1, stars: 4},
-    {id: 185, name: 'Time Mage', faction: 1, stars: 5},
-    {id: 217, name: 'Fat Mu', faction: 2, stars: 5},
-    {id: 218, name: 'Fat Mu', faction: 2, stars: 6},
-    {id: 225, name: 'Gusta', faction: 2, stars: 5},
-    {id: 226, name: 'Gusta', faction: 2, stars: 6},
-    {id: 288, name: "Demon Hunter", faction: 3, stars: 5},
-    {id: 289, name: "Demon Hunter", faction: 3, stars: 6},
-    {id: 290, name: "Demon Hunter", faction: 3, stars: 7},
-    {id: 291, name: "Demon Hunter", faction: 3, stars: 8},
-    {id: 292, name: "Demon Hunter", faction: 3, stars: 9},
-    {id: 293, name: "Demon Hunter", faction: 3, stars: 10},
-    {id: 343, name: 'Starlight', faction: 3, stars: 5},
-    {id: 344, name: 'Starlight', faction: 3, stars: 6},
-    {id: 349, name: 'Sybil', faction: 3, stars: 3},
-    {id: 350, name: 'Thale', faction: 3, stars: 4},
-    {id: 351, name: 'Thale', faction: 3, stars: 5},
-    {id: 352, name: 'Thale', faction: 3, stars: 6},
-    {id: 363, name: 'Wind Walker', faction: 3, stars: 4},
-    {id: 364, name: 'Wind Walker', faction: 3, stars: 5},
-    {id: 392, name: 'Logan', faction: 4, stars: 4},
-    {id: 393, name: 'Logan', faction: 4, stars: 5},
-    {id: 414, name: 'Fegan', faction: 5, stars: 4},
-    {id: 415, name: 'Fegan', faction: 5, stars: 5},
-  ];
-}
+import HeroSmartBar from 'HeroSmartBar.jsx';
 
 export default class HeroFilter extends React.Component
 {
@@ -78,42 +10,10 @@ export default class HeroFilter extends React.Component
   {
     super(props);
     this.state = {
-      heroes: [],
+      heroList: new HeroList([]),
       smartBarFilters: {text: '', namePatterns: [], factionToggles: {}, starToggles: {}},
     };
     this.handleFilterChange = this.handleFilterChange.bind(this);
-  }
-
-  filteredHeroes()
-  {
-    if (this.areFiltersEmpty()) {
-      return [];
-    }
-    return this.state.heroes.filter((hero) => {
-      if (!matchesAnyPattern(hero.name, this.state.smartBarFilters.namePatterns)) {
-        return false;
-      }
-      if (!matchesToggles(hero.faction, this.state.smartBarFilters.factionToggles)) {
-        return false;
-      }
-      if (!matchesToggles(hero.stars, this.state.smartBarFilters.starToggles)) {
-        return false;
-      }
-      return true;
-    });
-  }
-
-  areFiltersEmpty()
-  {
-    if (this.state.smartBarFilters.namePatterns.length > 0) {
-      return false;
-    } else if (Object.keys(this.state.smartBarFilters.factionToggles).length > 0) {
-      return false;
-    } else if (Object.keys(this.state.smartBarFilters.starToggles).length > 0) {
-      return false;
-    } else {
-      return true;
-    }
   }
 
   handleFilterChange(newFilters)
@@ -128,7 +28,7 @@ export default class HeroFilter extends React.Component
     fetch(heroesPath, {headers: {Accept: 'application/json'}, credentials: 'same-origin'})
       .then((res) => res.json())
       .then((json) => {
-        this.setState(() => ({heroes: json}));
+        this.setState(() => ({heroList: new HeroList(json)}));
       })
       .catch((ex) => {
         console.error('JSON parsing failed: ', ex);
@@ -140,7 +40,7 @@ export default class HeroFilter extends React.Component
     return (
       <div>
         <HeroSmartBar text={this.state.smartBarFilters.text} onFilterChange={this.handleFilterChange} />
-        <HeroBox heroes={this.filteredHeroes()} topURI={this.props.topURI} />
+        <HeroBox heroes={this.state.heroList.filteredHeroes(this.state.smartBarFilters)} topURI={this.props.topURI} />
       </div>
     );
   }
---
+++ app/javascript/HeroList.js
@@ -0,0 +1,109 @@
+export default class HeroList
+{
+  constructor(heroes)
+  {
+    this.heroes = heroes;
+  }
+
+  static matchesAnyPattern(str, patterns)
+  {
+    if (patterns.length < 1) {
+      return true;
+    }
+    return patterns.some((regexp) => str.match(regexp));
+  }
+
+  static matchesToggles(index, toggles)
+  {
+    if (Object.keys(toggles).length < 1) {
+      return true;
+    }
+    if (index in toggles) {
+      return toggles[index];
+    }
+    return false;
+  }
+
+  static areFiltersEmpty(filters)
+  {
+    if (filters.namePatterns.length > 0) {
+      return false;
+    } else if (Object.keys(filters.factionToggles).length > 0) {
+      return false;
+    } else if (Object.keys(filters.starToggles).length > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  filteredHeroes(filters)
+  {
+    if (HeroList.areFiltersEmpty(filters)) {
+      return [];
+    }
+    return this.heroes.filter((hero) => {
+      if (!HeroList.matchesAnyPattern(hero.name, filters.namePatterns)) {
+        return false;
+      }
+      if (!HeroList.matchesToggles(hero.faction, filters.factionToggles)) {
+        return false;
+      }
+      if (!HeroList.matchesToggles(hero.stars, filters.starToggles)) {
+        return false;
+      }
+      return true;
+    });
+  }
+
+  static testHeroList()
+  {
+    return [
+      {id: 13, name: "Blood Blade", faction: 0, stars: 5},
+      {id: 14, name: "Blood Blade", faction: 0, stars: 6},
+      {id: 15, name: "Blood Blade", faction: 0, stars: 7},
+      {id: 16, name: "Blood Blade", faction: 0, stars: 8},
+      {id: 17, name: "Blood Blade", faction: 0, stars: 9},
+      {id: 18, name: "Blood Blade", faction: 0, stars: 10},
+      {id: 23, name: 'Bonecarver', faction: 0, stars: 4},
+      {id: 24, name: 'Bonecarver', faction: 0, stars: 5},
+      {id: 25, name: 'Bonecarver', faction: 0, stars: 6},
+      {id: 32, name: 'Dark Priest', faction: 0, stars: 3},
+      {id: 82, name: 'Shirley', faction: 0, stars: 3},
+      {id: 103, name: 'Fire Fist', faction: 1, stars: 3},
+      {id: 117, name: "Iceblink", faction: 1, stars: 5},
+      {id: 118, name: "Iceblink", faction: 1, stars: 6},
+      {id: 119, name: "Iceblink", faction: 1, stars: 7},
+      {id: 120, name: "Iceblink", faction: 1, stars: 8},
+      {id: 121, name: "Iceblink", faction: 1, stars: 9},
+      {id: 122, name: "Iceblink", faction: 1, stars: 10},
+      {id: 161, name: 'Reggie', faction: 1, stars: 3},
+      {id: 180, name: 'Storm Hudde', faction: 1, stars: 4},
+      {id: 181, name: 'Storm Hudde', faction: 1, stars: 5},
+      {id: 184, name: 'Time Mage', faction: 1, stars: 4},
+      {id: 185, name: 'Time Mage', faction: 1, stars: 5},
+      {id: 217, name: 'Fat Mu', faction: 2, stars: 5},
+      {id: 218, name: 'Fat Mu', faction: 2, stars: 6},
+      {id: 225, name: 'Gusta', faction: 2, stars: 5},
+      {id: 226, name: 'Gusta', faction: 2, stars: 6},
+      {id: 288, name: "Demon Hunter", faction: 3, stars: 5},
+      {id: 289, name: "Demon Hunter", faction: 3, stars: 6},
+      {id: 290, name: "Demon Hunter", faction: 3, stars: 7},
+      {id: 291, name: "Demon Hunter", faction: 3, stars: 8},
+      {id: 292, name: "Demon Hunter", faction: 3, stars: 9},
+      {id: 293, name: "Demon Hunter", faction: 3, stars: 10},
+      {id: 343, name: 'Starlight', faction: 3, stars: 5},
+      {id: 344, name: 'Starlight', faction: 3, stars: 6},
+      {id: 349, name: 'Sybil', faction: 3, stars: 3},
+      {id: 350, name: 'Thale', faction: 3, stars: 4},
+      {id: 351, name: 'Thale', faction: 3, stars: 5},
+      {id: 352, name: 'Thale', faction: 3, stars: 6},
+      {id: 363, name: 'Wind Walker', faction: 3, stars: 4},
+      {id: 364, name: 'Wind Walker', faction: 3, stars: 5},
+      {id: 392, name: 'Logan', faction: 4, stars: 4},
+      {id: 393, name: 'Logan', faction: 4, stars: 5},
+      {id: 414, name: 'Fegan', faction: 5, stars: 4},
+      {id: 415, name: 'Fegan', faction: 5, stars: 5},
+    ];
+  }
+}
