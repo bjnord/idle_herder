c58cb4bd4211fa1713660d1481c36c8300c2de07
Brent J. Nordquist <brent@nordist.net>
2017-09-12 12:03:17 -0500

    RF: Extract HeroIcon component

---
+++ app/javascript/HeroIcon.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function heroImagePath(uri, hero)
+{
+  return uri + '/assets/heroes/' + hero.image_file;
+}
+
+export default function HeroIcon({ hero, topURI })
+{
+  return (<div className="image">
+    <img src={heroImagePath(topURI, hero)} height="60" width="60" />
+  </div>);
+}
