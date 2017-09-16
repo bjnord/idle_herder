29cc9888e2a6ea2df17b4379da110b7fa0ab5fb9
Brent J. Nordquist <brent@nordist.net>
2017-09-16 11:36:56 -0500

    RF: Fix spacing glitch

--- db/migrate/20170914111549_set_account_heroes_defaults.rb
+++ db/migrate/20170914111549_set_account_heroes_defaults.rb
@@ -1,6 +1,6 @@
 class SetAccountHeroesDefaults < ActiveRecord::Migration[5.1]
   def change
-     change_column_default :account_heroes, :level, from: nil, to: 1
-     change_column_default :account_heroes, :shards, from: nil, to: 0
+    change_column_default :account_heroes, :level, from: nil, to: 1
+    change_column_default :account_heroes, :shards, from: nil, to: 0
   end
 end
