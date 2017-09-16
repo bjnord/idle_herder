470c77dc96f2023ce1de83d1d3916a9f45cbce21
Brent J. Nordquist <brent@nordist.net>
2017-09-16 10:59:32 -0500

    Fix broken factory (factions 0..5 not 1..6)

--- spec/factories/material.rb
+++ spec/factories/material.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :material do
     association  :hero
     count        { 2 + rand(3) }
-    stars        { 4 + rand(2) }
-    faction      { 1 + rand(6) }
+    stars        { 4 + rand(3) }
+    faction      { rand(6) }
   end
 end
