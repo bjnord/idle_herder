678dbd19c332e56da00f9c285ee6895f32196072
Brent J. Nordquist <brent@nordist.net>
2017-09-18 06:39:40 -0500

    RF: Better set-initial code for previous migration
    yes I tested it

--- db/migrate/20170916163023_set_account_heroes_is_fodder_default.rb
+++ db/migrate/20170916163023_set_account_heroes_is_fodder_default.rb
@@ -2,7 +2,14 @@ class SetAccountHeroesIsFodderDefault < ActiveRecord::Migration[5.1]
   def change
     change_column_default :account_heroes, :is_fodder, from: nil, to: false
     reversible do |direction|
-      direction.up { AccountHero.all.each {|ah| ah.is_fodder ||= false ; ah.wish_list=true if ah.wish_list? ; ah.save } }
+      direction.up do
+        $stderr.puts "-- setting is_fodder for existing account_heroes"
+        AccountHero.all.each do |ah|
+          ah.is_fodder ||= false
+          ah.wish_list = true if ah.wish_list?
+          ah.save!
+        end
+      end
     end
     change_column_null :account_heroes, :is_fodder, false
   end
