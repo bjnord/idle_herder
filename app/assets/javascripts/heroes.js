43dba97f1f0ac6e2c066d98c0e793abc1c59bdd8
Brent J. Nordquist <brent@nordist.net>
2017-09-19 06:43:46 -0500

    Also disable Add button until hero selected

--- app/assets/javascripts/heroes.js
+++ app/assets/javascripts/heroes.js
@@ -3,6 +3,7 @@ function setSelectedHero(hero)
 {
   $('#account_hero_hero_id').val(hero.id);
   $('#account-hero-fields :input').prop('disabled', false);
+  $('.actions :input[type=submit]').prop('disabled', false);
   for (var i = 1; i <= 10; i++) {
     if ((i < hero.stars) || (i > hero.max_stars)) {
       $('#account_hero_target_stars_' + i).prop('checked', false)
@@ -20,6 +21,7 @@ function unsetSelectedHero()
 {
   $('#account_hero_hero_id').val(null);
   $('#account-hero-fields :input').prop('disabled', true);
+  $('.actions :input[type=submit]').prop('disabled', true);
   // carefully avoid disturbing the magic hidden checkbox inputs:
   $('#account-hero-fields input[type=text]').val(null);
   $('#account-hero-fields input[type=radio]').prop('checked', false);
