735af56bc12f96a3006fc41e9bdbc5b5220a7939
Brent J. Nordquist <brent@nordist.net>
2017-09-21 17:31:39 -0500

    Get rid of ShardType static data
    prep for the "great refactor"

--- db/seeds/050_shard_types.rb
+++
@@ -1,8 +0,0 @@
-shard_type_count = Dir[Rails.root.join('db', 'data', 'shard_types', '*')].count {|f| File.file?(f) }
-$stderr.puts "seeding #{shard_type_count} shard_types"
-ShardType.destroy_all
-(1..shard_type_count).each do |id|
-  h = ShardType.build_from_json(id)
-  h.save!
-end
-$stderr.puts "ShardType.count=#{ShardType.count}"
